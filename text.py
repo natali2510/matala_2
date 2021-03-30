bc4b613d7062c90b793a1efa753aa731617f8f5d
natali2510 <nataligermay60@gmail.com>
2021-03-30 21:18:32 +0300

    final code

---
+++ text.py
@@ -0,0 +1,18 @@
+def revword(word):
+    return word[::-1].lower()
+
+
+def countword():
+    cnt=1
+    txt= open('text.txt','r')
+    txt=txt.read()
+    txt=txt.replace('\n',' ') 
+    txt=(txt.lower())
+    word1=txt.split()
+    for i in range(1,len(word1)):
+        if word1[0]==revword(word1[i]):
+           cnt = cnt + 1 
+    print(cnt)
+countword()
+
+    
